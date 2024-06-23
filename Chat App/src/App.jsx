3e7b8da24f44ffefc756fccd4bd95c23cbd48d52
ManManavadaria <manmanavadaria@gmail.com>
2024-06-23 17:38:18 +0530

    update chat.go import in app.js

--- Chat App/src/App.jsx
+++ Chat App/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import Chat from './pages/chat';
+import Chat from './pages/Chat';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import SetAvatar from './component/SetAvatar';
